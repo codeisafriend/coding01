06d3fe52402f407f2bba96b89f370eac8ec4c639
takeoarai <takeo.60271.arai@gmail.com>
2024-04-13 21:44:59 +0900

    Added necessary images

--- app/page.js
+++ app/page.js
@@ -1,113 +1,201 @@
 import Image from "next/image";
 
+import logoImage from "@/public/images/logo.svg";
+import mainvisualImage from "@/public/images/mainvisual.jpg";
+import aboutImage from "@/public/images/about.jpg";
+import bicycle1Image from "@/public/images/bicycle1.jpg";
+import bicycle2Image from "@/public/images/bicycle2.jpg";
+import bicycle3Image from "@/public/images/bicycle3.jpg";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <>
+      {/* <header className="max-w-[960px] flex justify-between p-[2%] mr-[auto] ml-[auto]">
+        <h1>
+          <Image
+            src={logoImage}
+            alt="ロゴ画像"
+            className="w-[100%] h-10 align-bottom"
+            priority
+          />
+        </h1>
+        <nav>
+          <ul className="flex justify-center justify-items-center">
+            <li className="pr-10">About</li>
+            <li>Bicycle</li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <section className="mb-16">
+          <div className="w-[100%]">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
+              src={mainvisualImage}
+              alt="mainvisual 壁に立てかけられた自転車"
+              className="max-w-full h-[600px] align-bottom object-cover"
               priority
             />
-          </a>
+          </div>
+        </section>
+        <section className="max-w-[960px] text-[#383E45] px-[4%]">
+          <h2 className="font-bold text-3xl text-center mb-16">About</h2>
+          <div className="flex flex-col justify-center items-center">
+            <div className="w-[100%]">
+              <Image
+                src={aboutImage}
+                alt="KAKERU MIYAICHIさんを彷彿とさせる抽象的人物"
+                className="max-w-full w-[100px] h-[100px] align-bottom mr-[auto] ml-[auto] rounded-[50%]"
+                priority
+              />
+            </div>
+            <div className="text-left mr-[auto] ml-[auto] flex">
+              <h3 className="font-bold text-base my-3">KAKERU MIYAICHI</h3>
+              <p className="text-sm">
+                テキストテキストテキストテキストテキストテキストテキスト
+                テキストテキストテキストテキストテキストテキストテキスト
+                テキストテキストテキストテキストテキストテキストテキスト
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="max-w-[960px] text-[#383E45] p-[4%] mr-[auto] ml-[auto]">
+          <h2 className="font-bold text-3xl text-center">Bicycle</h2>
+          <div>
+            <Image src={bicycle1Image} alt="自転車1" priority />
+            <h3>タイトルタイトル</h3>
+            <p>テキストテキストテキスト</p>
+          </div>
+          <div>
+            <Image src={bicycle2Image} alt="自転車2" priority />
+            <h3>タイトルタイトル</h3>
+            <p>テキストテキストテキスト</p>
+          </div>
+          <div>
+            <Image src={bicycle3Image} alt="自転車3" priority />
+            <h3>タイトルタイトル</h3>
+            <p>テキストテキストテキスト</p>
+          </div>
+        </section>
+      </main>
+
+      <footer>
+        <p>&copy; 2020 Profile</p>
+      </footer> */}
+
+      <header className="w-[100%]">
+        <div className="flex p-4 items-center justify-between max-w-[960px] mr-[auto] ml-[auto]">
+          <h1>
+            <Image
+              src={logoImage}
+              alt="ロゴ画像"
+              className="w-[100%] h-10 align-bottom"
+              priority
+            />
+          </h1>
+          <nav className="">
+            <ul className="flex text-[#24292E]">
+              <li>About</li>
+              <li className="pl-6">Bicycle</li>
+            </ul>
+          </nav>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+      </header>
+      <main>
+        <section className="h-[600px] block relative w-[100%]">
+          <div className="w-[100%]">
+            <Image
+              src={mainvisualImage}
+              alt="mainvisual 壁に立てかけられた自転車"
+              className="max-w-full h-[600px] align-bottom object-cover"
+              priority
+            />
+          </div>
+        </section>
+        <section className="w-[96%] max-w-[960px] pt-24 mr-[auto] ml-[auto] px-[2%]">
+          <h2 className="text-center text-4xl font-bold text-[#383E45] relative after:content-[''] after:-top-0 after:left-0 after:border-b after:border-gray-900 pb-24">
+            About
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div className="sm:flex">
+            <Image
+              src={aboutImage}
+              alt="KAKERU MIYAICHIさんを彷彿とさせる抽象的人物"
+              width={100}
+              height={100}
+              className="w-[100px] h-[100px] object-cover rounded-[50%] mr-[auto] ml-[auto]"
+            />
+            <div className="text-[#383E45]">
+              <h3 className="text-xl font-bold py-3">KAKERU MIYAICHI</h3>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+              <p className="text-sm">
+                テキストテキストテキストテキストテキストテキストテキスト
+              </p>
+              <p className="text-sm">
+                テキストテキストテキストテキストテキストテキストテキスト
+              </p>
+              <p className="text-sm">
+                {" "}
+                テキストテキストテキストテキストテキストテキストテキスト
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="w-[96%] max-w-[960px] pt-24 mr-[auto] ml-[auto] px-[2%]">
+          <h2 className="text-center text-4xl font-bold text-[#383E45] relative after:content-[''] after:-top-0 after:left-0 after:border-b after:border-gray-900 pb-24">
+            Bicycle
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <div className="sm:flex">
+            <div>
+              <Image
+                src={bicycle1Image}
+                alt="自転車1"
+                width={600}
+                height={300}
+              />
+              <h3 className="text-center pt-3 font-semibold text-[#383E45]">
+                タイトルタイトル
+              </h3>
+              <p className="text-center pt-4 text-sm text-[#383E45]">
+                テキストテキストテキスト
+              </p>
+            </div>
+            <div className="sm:ml-3">
+              <Image
+                src={bicycle2Image}
+                alt="自転車2"
+                width={600}
+                height={300}
+                className="mt-9 sm:mt-0"
+              />
+              <h3 className="text-center pt-3 font-semibold text-[#383E45]">
+                タイトルタイトル
+              </h3>
+              <p className="text-center pt-4 text-sm text-[#383E45]">
+                テキストテキストテキスト
+              </p>
+            </div>
+            <div className="sm:ml-3">
+              <Image
+                src={bicycle3Image}
+                alt="自転車3"
+                width={600}
+                height={300}
+                className="mt-9 sm:mt-0"
+              />
+              <h3 className="text-center pt-3 font-semibold text-[#383E45]">
+                タイトルタイトル
+              </h3>
+              <p className="text-center pt-4 text-sm text-[#383E45]">
+                テキストテキストテキスト
+              </p>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer className="mt-48 max-w-[960px] pt-24 mr-[auto] ml-[auto]">
+        <p className="text-center text-[0.65rem]">&copy; 2020 Profile</p>
+      </footer>
+    </>
   );
 }
